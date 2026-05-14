2ec02e1e0d2e98a2b772102924cebce49f622b2e
Axel Torres <aj@AJs-Macbook.local>
2026-05-13 23:23:30 -0400

    Revert no-match account-scoped location lookup

--- src/no-match-cloud.js
+++ src/no-match-cloud.js
@@ -12,10 +12,10 @@
   }
 
   async function fetchLocationId() {
-    const { url, key, accountId, locationName } = getConfig();
+    const { url, key, locationName } = getConfig();
 
     const res = await fetch(
-      `${url}/rest/v1/locations?account_id=eq.${encodeURIComponent(accountId)}&name=eq.${encodeURIComponent(locationName)}&select=id`,
+      `${url}/rest/v1/locations?name=eq.${encodeURIComponent(locationName)}&select=id`,
       {
         headers: {
           apikey: key,
