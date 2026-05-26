0a899798251843f6f3a1d54034e5278c4752ae3b
Axel Torres <axel@crowninconcord.com>
2026-05-25 23:18:21 -0400

    chore: version bump v1.3

--- scripts/update_build.py
+++ scripts/update_build.py
@@ -15,7 +15,7 @@ html = re.sub(
     flags=re.DOTALL
 )
 
-target = '<div class="brand-version">v1.0</div>'
+target = '<div class="brand-version">v1.3</div>'
 replacement = f'{target}\n<div id="buildBadge" style="font-size:12px;font-weight:800;opacity:.7;margin-top:4px;">{build}</div>'
 
 if target not in html:
